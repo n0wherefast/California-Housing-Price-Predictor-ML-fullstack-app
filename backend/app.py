7d0086f27bd9758cfd96e8502ae3e03dd9f9a684
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2025-11-03 17:19:27 +0100

    Fix: use dynamic port for Render

--- backend/app.py
+++ backend/app.py
@@ -131,4 +131,6 @@ def eda():
 
 # Avvio server
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+
